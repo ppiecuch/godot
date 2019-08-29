3771e02899dc50edd78003f46213040331a2ed3c
Pawel Piecuch <pawel.piecuch@roche.com>
2019-08-29 23:09:10 +0200

    organizing submodules

--- submodules.sh
+++ submodules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for m in gd_sqlite gd_chipmunk gd_liquidfun gd_spine; do
+for m in gd_sqlite gd_chipmunk gd_liquidfun gd_spine gd_error_handler; do
 	if [ -e modules/$m ]; then
 		git update modules/$m
 	else
