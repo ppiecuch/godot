854e0da3d33d9b15eca0083ae55f0de1397837e8
ppiecuch <piecuch.pawel@gmail.com>
2019-09-01 00:36:50 +0200

    update for simple win32 bash

--- submodules.sh
+++ submodules.sh
@@ -3,7 +3,7 @@
 for m in gd_sqlite gd_chipmunk gd_liquidfun gd_spine gd_vector_graphics gd_error_handler; do
     echo "*** checking module $m"
     if [ -e modules/$m/.git ]; then
-        ( cd modules/$m; git pull )
+        pushd modules/$m; git pull; popd
     else
         git clone https://github.com/ppiecuch/$m modules/$m
     fi
