4cebcf178bc82cabbeb3d1e7ce703cd56bebcd22
ppiecuch <piecuch.pawel@gmail.com>
2019-09-01 02:09:48 +0200

    cleanup win32 build

--- submodules.sh
+++ submodules.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 for m in gd_sqlite gd_chipmunk gd_liquidfun gd_spine gd_vector_graphics gd_error_handler; do
+    echo "***"
     echo "*** checking module $m"
+    echo "***"
     if [ -e modules/$m/.git ]; then
         pushd modules/$m; git pull; popd
     else
-        git clone https://github.com/ppiecuch/$m modules/$m
+        git clone --recursive https://github.com/ppiecuch/$m modules/$m
     fi
 done
