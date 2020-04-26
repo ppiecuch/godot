f43911ef1c5f011c364ac53203461560bca02006
Pawel Piecuch <pawel.piecuch@roche.com>
2020-04-26 22:36:31 +0200

    added bullet hell module

---
+++ modules/gd_bullet_hell/register_types.cpp
@@ -0,0 +1,16 @@
+/* register_types.cpp */
+
+#include <core/print_string.h>
+#include <scene/2d/canvas_item.h>
+
+#include "register_types.h"
+
+void register_gd_bullet_hell_types()
+{
+
+}
+
+void unregister_gd_bullet_hell_types()
+{
+
+}
---
+++ modules/gd_bullet_hell/register_types.h
@@ -0,0 +1,4 @@
+/* register_types.h */
+
+void register_gd_bullet_hell_types();
+void unregister_gd_bullet_hell_types();
