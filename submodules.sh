a9a241f3f2b566fb7db2ad1f056cc4697c7ab087
Pawel Piecuch <pawel.piecuch@roche.com>
2020-05-26 08:16:40 +0200

    added new modules

--- submodules.sh
+++ submodules.sh
@@ -5,7 +5,7 @@
 
 set -e
 
-for m in gd_distrand gd_sqlite gd_unqlite gd_chipmunk gd_liquidfun gd_spine gd_vector_graphics gd_error_handler gd_raknet gd_enet; do
+for m in gd_distrand gd_sqlite gd_unqlite gd_chipmunk gd_liquidfun gd_spine gd_vector_graphics gd_error_handler gd_raknet gd_enet gd_imagetools gd_geomtools; do
 	echo "+--"
     echo "| checking module: $m"
     echo "+--"
