daa3b4559d16646f0e43bb618ddf0ac34d346c79
Pawel Piecuch <pawel.piecuch@roche.com>
2020-09-18 12:49:21 +0200

    improve frt building issues

--- thirdparty/libvpx/vpx_config.h
+++ thirdparty/libvpx/vpx_config.h
@@ -47,8 +47,13 @@
 	#define ARCH_X86_64 0
 
 	#define ARCH_ARM 1
+#ifdef WEBM_NO_ARM_NEON
+	#define HAVE_NEON 0
+	#define HAVE_NEON_ASM 0
+#else
 	#define HAVE_NEON 1
 	#define HAVE_NEON_ASM 1
+#endif
 #elif defined(__aarch64__)
 	#define ARCH_X86 0
 	#define ARCH_X86_64 0
