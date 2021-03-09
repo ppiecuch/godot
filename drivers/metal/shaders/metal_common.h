97649d23095a7e50ac3f27277f7832e55af19a1f
Pawel Piecuch <pawel.piecuch@roche.com>
2021-03-09 10:19:24 +0100

    small cleanup in metal driver

--- drivers/metal/shaders/metal_common.h
+++ drivers/metal/shaders/metal_common.h
@@ -25,6 +25,35 @@ static float linstep( float low, float high, float v )
     return clamp( (v - low) / (high - low), 0.0f, 1.0f );
 }
 
+static float random(float2 p)
+{
+    return fract(sin(dot(p, float2(15.79, 81.93)) * 45678.9123));
+}
+
+static float noise(float2 p)
+{
+    float2 i = floor(p);
+    float2 f = fract(p);
+    f = f * f * (3.0 - 2.0 * f);
+    float bottom = mix(random(i + float2(0)), random(i + float2(1.0, 0.0)), f.x);
+    float top = mix(random(i + float2(0.0, 1.0)), random(i + float2(1)), f.x);
+    float t = mix(bottom, top, f.y);
+    return t;
+}
+
+static float fbm(float2 uv)
+{
+    float sum = 0;
+    float amp = 0.7;
+    for(int i = 0; i < 4; ++i)
+    {
+        sum += noise(uv) * amp;
+        uv += uv * 1.2;
+        amp *= 0.4;
+    }
+    return sum;
+}
+
 
 // Shaders
 
