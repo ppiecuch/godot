124cd95be9dfa04478b374a17046a555a2ef6b82
Pawel Piecuch <pawel.piecuch@roche.com>
2021-03-30 18:49:31 +0200

    update video drivers interface

--- platform/3ds/video/rasterizer_citro3d.h
+++ platform/3ds/video/rasterizer_citro3d.h
@@ -717,7 +717,7 @@ public:
 	void render_target_set_position(RID p_render_target, int p_x, int p_y) {}
 	void render_target_set_size(RID p_render_target, int p_width, int p_height) {}
 	RID render_target_get_texture(RID p_render_target) const { return RID(); }
-	void render_target_set_external_texture(RID p_render_target, unsigned int p_texture_id) {}
+	void render_target_set_external_texture(RID p_render_target, unsigned int p_texture_id, unsigned int p_depth_id) {}
 	void render_target_set_flag(RID p_render_target, RenderTargetFlags p_flag, bool p_value) {}
 	bool render_target_was_used(RID p_render_target) { return false; }
 	void render_target_clear_used(RID p_render_target) {}
