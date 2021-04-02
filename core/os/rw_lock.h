fa7c82f7e51955916c977b0e74b6c17efd8b595a
Pawel Piecuch <pawel.piecuch@roche.com>
2021-04-02 08:55:31 +0200

    fixing build issues; cleanup gcw building

--- core/os/rw_lock.h
+++ core/os/rw_lock.h
@@ -40,7 +40,7 @@
 
 #include PLATFORM_RWLOCK_H
 
-#elif ((defined(_MSVC_LANG) && _MSVC_LANG < 201402L) || __cplusplus < 201402L)
+#elif defined(UNIX_ENABLED) && __cplusplus < 201402L
 
 #include "drivers/posix/rw_lock_posix.h"
 
