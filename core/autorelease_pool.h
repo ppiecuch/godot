19ad86685030d70a5073fae8697023324d187a82
Pawel Piecuch <pawel.piecuch@roche.com>
2021-05-29 20:21:40 +0200

    small objc utilities

--- core/autorelease_pool.h
+++ core/autorelease_pool.h
@@ -1,28 +1,57 @@
+/*************************************************************************/
+/*  autorelease_pool.h                                                   */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
 #ifndef AUTORELEASE_POOL_H
 #define AUTORELEASE_POOL_H
 
 #ifdef __OBJC__
-typedef NSAutoreleasePool* NSAutoreleasePoolPtr;
+typedef NSAutoreleasePool *NSAutoreleasePoolPtr;
 #else
-# include <objc/NSObjCRuntime.h>
+#include <objc/NSObjCRuntime.h>
 using NSAutoreleasePoolPtr = id;
 #endif
 
-class AutoreleasePool final
-{
+class AutoreleasePool final {
 public:
 	AutoreleasePool() noexcept;
 	~AutoreleasePool();
-	AutoreleasePool(const AutoreleasePool& other) noexcept;
-	AutoreleasePool(AutoreleasePool&& other) noexcept;
-	AutoreleasePool& operator=(const AutoreleasePool& other) noexcept;
-	AutoreleasePool& operator=(AutoreleasePool&& other) noexcept;
+	AutoreleasePool(const AutoreleasePool &other) noexcept;
+	AutoreleasePool(AutoreleasePool &&other) noexcept;
+	AutoreleasePool &operator=(const AutoreleasePool &other) noexcept;
+	AutoreleasePool &operator=(AutoreleasePool &&other) noexcept;
 
-	bool operator==(const AutoreleasePool& other) const noexcept {
+	bool operator==(const AutoreleasePool &other) const noexcept {
 		return pool == other.pool;
 	}
 
-	bool operator!=(const AutoreleasePool& other) const noexcept {
+	bool operator!=(const AutoreleasePool &other) const noexcept {
 		return pool != other.pool;
 	}
 
--- core/autorelease_pool.mm
+++ core/autorelease_pool.mm
@@ -1,34 +1,65 @@
+/*************************************************************************/
+/*  autorelease_pool.mm                                                  */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
 #if defined(OSX_ENABLED) || defined(IOS_ENABLED)
 
 #include "autorelease_pool.h"
 
-AutoreleasePool::AutoreleasePool() noexcept:
-	pool([[NSAutoreleasePool alloc] init]) {
+AutoreleasePool::AutoreleasePool() noexcept :
+		pool([[NSAutoreleasePool alloc] init]) {
 }
 
 AutoreleasePool::~AutoreleasePool() {
 	[pool release];
 }
 
-AutoreleasePool::AutoreleasePool(const AutoreleasePool& other) noexcept:
-	pool([other.pool retain]) {
+AutoreleasePool::AutoreleasePool(const AutoreleasePool &other) noexcept :
+		pool([other.pool retain]) {
 }
 
-AutoreleasePool::AutoreleasePool(AutoreleasePool&& other) noexcept:
-	pool(other.pool)
-{
+AutoreleasePool::AutoreleasePool(AutoreleasePool &&other) noexcept :
+		pool(other.pool) {
 	other.pool = nil;
 }
 
-AutoreleasePool& AutoreleasePool::operator=(const AutoreleasePool& other) noexcept {
-	if (&other == this) return *this;
+AutoreleasePool &AutoreleasePool::operator=(const AutoreleasePool &other) noexcept {
+	if (&other == this)
+		return *this;
 	[pool release];
 	pool = [other.pool retain];
 	return *this;
 }
 
-AutoreleasePool& AutoreleasePool::operator=(AutoreleasePool&& other) noexcept {
-	if (&other == this) return *this;
+AutoreleasePool &AutoreleasePool::operator=(AutoreleasePool &&other) noexcept {
+	if (&other == this)
+		return *this;
 	[pool release];
 	pool = other.pool;
 	other.pool = nil;
