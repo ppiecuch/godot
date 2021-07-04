3dffe32b4a725d8df7fc13e1ca1f0bd964e768ec
Pawel Piecuch <pawel.piecuch@roche.com>
2021-07-04 16:09:06 +0200

    cleanup some file naming; update descriptions

--- modules/gdextensions/gd/godot_error_handler.cpp
+++ modules/gdextensions/gd/godot_error_handler.cpp
@@ -99,6 +99,52 @@ void GodotErrorHandler::_err_handler(void *ud, const char *p_func, const char *p
 	_lastError = _error;
 	_lastErrorCnt = 1;
 
-	// 	GodotErrorHandler *gdh = (GodotErrorHandler *)ud;
+	// GodotErrorHandler *gdh = (GodotErrorHandler *)ud;
 	// _print_format_error(errorObject);
 }
+
+#if 0
+
+# Godot Error Handler (from https://github.com/binogure-studio/godot-error-handler)
+
+Simple error handler module for Godot Engine
+
+## Installation
+
+Copy `godoterrorhandler` to the `godot/modules` folder. Then [compile Godot](http://docs.godotengine.org/en/2.1/development/compiling/introduction_to_the_buildsystem.html)
+
+## Usage
+
+### Engine.cfg
+
+```
+[autoload]
+
+errorhandler="*res://scripts/errorhandler.gd"
+```
+
+### Example
+
+```gdscript
+extends node
+
+func _ready():
+  GodotErrorHandler.connect('error_threw', self, 'error_handler')
+
+func error_handler(dump = null):
+  # Dump contains following informations
+  #	dump["error"] name
+  #	dump["error_descr"] description
+  #	dump["source_file"] file
+  #	dump["source_line"] line number
+  #	dump["source_func"] function name
+  #	dump["warning"] = true/false
+  #	dump["callstack"] = callstack (only in debug)
+  print(dump)
+```
+
+# License
+
+[See LICENSE file](./LICENSE)
+
+#endif
