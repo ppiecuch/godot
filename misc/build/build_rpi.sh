60509fe38b67a1049edfba8e5eeb5550258ec745
Pawel Piecuch <pawel.piecuch@roche.com>
2021-09-24 20:10:02 +0200

    additional build scripts

---
+++ misc/build/build_rpi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export RPXC_IMAGE="rpi_toolchain"
+
+docker run ${RPXC_IMAGE} > ./bin/rpxc
+chmod +x ./bin/rpxc
+echo "*** Using gcc/toolchain version $(./bin/rpxc -- arm-linux-gnueabihf-gcc --version)"
+
+device="pi3"
+
+if [ ! -z "$1" ]; then
+	device="$1"
+	echo "*** Using device: ${device}"
+	pause 3
+fi
+
+./bin/rpxc -- scons -j2 platform=frt frt_arch=${device} verbose=yes warnings=all werror=yes target=release tools=no
