3c66eae199e02deb0a2689ebf4ae9eec0ef44ac1
Pawel Piecuch <pawel.piecuch@roche.com>
2021-09-25 20:23:03 +0200

    added core count to the script

--- misc/build/build_win.cmd
+++ misc/build/build_win.cmd
@@ -6,15 +6,15 @@ SETLOCAL
 
 PATH=%PATH%;%CD%\bin
 SET GODOT_DIR=%~dp0
-ECHO *** Building 64-bit default editor for Windows ..
-CALL scons platform=windows tools=yes use_lto=yes || GOTO END
+ECHO *** Building 64-bit default editor for Windows (core: %NUMBER_OF_PROCESSORS%) ..
+CALL scons -j%NUMBER_OF_PROCESSORS% platform=windows tools=yes use_lto=yes || GOTO END
 
 IF NOT "%1"=="templates" GOTO END
     ECHO *** Building 64-bit debug export template for Windows ..
-    CALL scons platform=windows target=release_debug bits=64 tools=no use_lto=yes || GOTO END
+    CALL scons -j%NUMBER_OF_PROCESSORS% platform=windows target=release_debug bits=64 tools=no use_lto=yes || GOTO END
 
     ECHO *** Building 64-bit release export template for Windows ..
-    CALL scons platform=windows target=release bits=64 tools=no use_lto=yes || GOTO END
+    CALL scons -j%NUMBER_OF_PROCESSORS% platform=windows target=release bits=64 tools=no use_lto=yes || GOTO END
 
     CALL strip "%GODOT_DIR%\bin\godot.windows.opt.debug.64.exe" ^
         "%GODOT_DIR%\bin/\odot.windows.opt.64.exe"
