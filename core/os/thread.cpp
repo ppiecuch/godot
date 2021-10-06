2f0d4ce7dd21af8d54caf737539883aaa31c9061
Pawel Piecuch <pawel.piecuch@roche.com>
2021-10-06 23:23:31 +0200

    cleanup code formatting

--- core/os/thread.cpp
+++ core/os/thread.cpp
@@ -28,7 +28,6 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-
 #include "thread.h"
 
 #ifndef PLATFORM_THREAD_H // // See details in thread.h
