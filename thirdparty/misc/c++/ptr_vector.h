0c5ab5c6695e153b1e5de22a64e06026035ac905
Pawel Piecuch <pawel.piecuch@roche.com>
2021-10-06 08:41:21 +0200

    cleanup commenting

--- thirdparty/misc/c++/ptr_vector.h
+++ thirdparty/misc/c++/ptr_vector.h
@@ -1,31 +1,38 @@
 #ifndef PTR_VECTOR_H
 #define PTR_VECTOR_H
 
-/** @file
-<pre>
-  Copyright (c) 2004, 2005, 2006 Roland Pibinger
-  All rights reserved.
+//   Copyright (c) 2004, 2005, 2006 Roland Pibinger
+//   All rights reserved.
+//
+//   Redistribution and use in source and binary forms, with or without modification,
+//   are permitted provided that the following conditions are met:
+//
+//   - Redistributions of source code must retain the above copyright notice, this
+//     list of conditions and the following disclaimer.
+//   - Neither the name of the copyright holders nor the names of contributors may
+//     be used to endorse or promote products derived from this software without
+//     specific prior written permission.
+//
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+//   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+//   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-  Redistribution and use in source and binary forms, with or without modification,
-  are permitted provided that the following conditions are met:
-
-  - Redistributions of source code must retain the above copyright notice, this
-    list of conditions and the following disclaimer.
-  - Neither the name of the copyright holders nor the names of contributors may
-    be used to endorse or promote products derived from this software without
-    specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-</pre>
+/*
+ * ptr_vector - A Container For Pointers
+ *              Convenient STL-compliant vector for pointers
+ * ptr_vector<T> is a wrapper for Standard vector<T*> that cuts one level of
+ * indirection. In essence ptr_vector lets you treat a vector of pointers as if
+ * it were a vector of values.
+ *
+ *     for a detailed description see
+ *     http://www.codeproject.com/vcpp/stl/ptr_vecto.asp
  *
  * @email rpbg123@yahoo.com
  *
@@ -39,19 +46,6 @@
  * @date     25-Oct-2006 - chg: ptr_vec_iter_imp: assert in op_pp() and op_mm() corrected
  */
 
-/*
- *
- *
- *  ptr_vector - A Container For Pointers
- *               Convenient STL-compliant vector for pointers
- * ptr_vector<T> is a wrapper for Standard vector<T*> that cuts one level of
- * indirection. In essence ptr_vector lets you treat a vector of pointers as if
- * it were a vector of values.
- *
- *     for a detailed description see
- *     http://www.codeproject.com/vcpp/stl/ptr_vecto.asp
- *
-*/
 
 #if defined(_MSC_VER) && _MSC_VER <= 1200 // MSVC++ 6.0
 // disable warning C4786: identifier longer than 255 characters (identifier was truncated to '255' characters)
