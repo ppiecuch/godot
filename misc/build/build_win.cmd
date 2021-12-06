30070da15ef228f4b684cb959fd24912b4de41a7
Pawel Piecuch <pawel.piecuch@roche.com>
2021-12-06 16:24:57 +0100

    cleanup and improve win32 build

--- misc/build/build_win.cmd
+++ misc/build/build_win.cmd
@@ -9,8 +9,8 @@ SETLOCAL
 
 PATH=%PATH%;%CD%\bin;%CD%\misc\tools
 SET GODOT_DIR=%~dp0
-ECHO *** Building 64-bit default editor for Windows (core: %NUMBER_OF_PROCESSORS%) ..
-CALL scons -j%NUMBER_OF_PROCESSORS% platform=windows tools=yes use_lto=yes || GOTO END
+ECHO *** Building default editor for Windows (core: %NUMBER_OF_PROCESSORS%) ..
+CALL scons -j%NUMBER_OF_PROCESSORS% platform=windows tools=yes || GOTO END
 
 IF NOT "%1"=="templates" GOTO END
     ECHO *** Building 64-bit debug export template for Windows ..
