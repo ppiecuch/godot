ba1d6f5c450386ed63f1f63d4e725ac0fff9347d
Pawel Piecuch <pawel.piecuch@roche.com>
2021-12-07 11:59:20 +0100

    cleanup wrong syntax; add some color info

--- misc/scripts/check_custom_modules.sh
+++ misc/scripts/check_custom_modules.sh
@@ -5,9 +5,9 @@ set -e
 (cd modules; for m in $(ls -d gd_*|xargs); do
 	(cd $m; if [ -e .git ]; then
 		echo "+---"
-		echo "| checking module $m ($(git remote get-url origin))"
+		echo -e "| checking module \033[1;4m$m\033[0m ($(git remote get-url origin))"
 		echo "+---"
-		git status $m | grep -v "nothing to commit" | grep .
+		git status -uno | grep -v "nothing to commit" | grep .
 		echo ""
 	fi)
 done)
