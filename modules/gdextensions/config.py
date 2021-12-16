68e72065d9ef35029dc233b68ad000aa11888d93
Pawel Piecuch <pawel.piecuch@roche.com>
2021-12-16 17:29:10 +0100

    add option for enabling submodules in gdextensions

--- modules/gdextensions/config.py
+++ modules/gdextensions/config.py
@@ -3,7 +3,25 @@ def can_build(env, platform):
 
 
 def configure(env):
-    pass
+    from SCons.Script import Variables, ListVariable, Help, Exit
+
+    modules = [
+        "core",
+        "bulletkit",
+        "visual",
+        "environment",
+        "smooth",
+        "debugdraw",
+        "statemachine",
+        "benet",
+        "behaviornode",
+        "settings",
+        "sfxr",
+        "vgamepad",
+    ]
+    opts = Variables()
+    opts.Add(ListVariable("enable_gdextensions_submodules", "Enable gdextensions submodules", "all", modules))
+    opts.Update(env)
 
 
 def get_doc_path():
