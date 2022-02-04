786ae7c209d83a481b87668bf746ab4da6ba504e
Pawel Piecuch <pawel.piecuch@roche.com>
2022-02-04 14:24:30 +0100

    cleanup conflicting issues

--- thirdparty/mbedtls/library/entropy_poll.c
+++ thirdparty/mbedtls/library/entropy_poll.c
@@ -46,7 +46,7 @@
 
 #if !defined(unix) && !defined(__unix__) && !defined(__unix) && \
     !defined(__APPLE__) && !defined(_WIN32) && !defined(__QNXNTO__) && \
-    !defined(__HAIKU__) && && !defined(__midipix__) \
+    !defined(__HAIKU__) && !defined(__midipix__) \
     !defined(_3DS) && !defined(__psp__) && !defined(__psp2__)
 #error "Platform entropy sources only work on Unix and Windows, see MBEDTLS_NO_PLATFORM_ENTROPY in config.h"
 #endif
