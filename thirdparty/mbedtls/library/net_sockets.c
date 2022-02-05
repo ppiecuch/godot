6922d2d1151989bd8dfd62a249eb329bfd4f78ca
Pawel Piecuch <pawel.piecuch@roche.com>
2022-02-05 10:31:56 +0100

    cleanup conflicting issues

--- thirdparty/mbedtls/library/net_sockets.c
+++ thirdparty/mbedtls/library/net_sockets.c
@@ -33,7 +33,7 @@
 
 #if !defined(unix) && !defined(__unix__) && !defined(__unix) && \
     !defined(__APPLE__) && !defined(_WIN32) && !defined(__QNXNTO__) && \
-    !defined(__HAIKU__) && !defined(__midipix__) \
+    !defined(__HAIKU__) && !defined(__midipix__) && \
     !defined(_3DS)  && !defined(__psp__) && !defined(__psp2__)
 #error "This module only works on Unix and Windows, see MBEDTLS_NET_C in config.h"
 #endif
