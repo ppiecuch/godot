efedcbca94f2ea424921d79abe66bafb81c29bfe
Pawel Piecuch <piecuch.pawel@gmail.com>
2022-03-08 20:38:32 +0100

    cleanup code formatting

--- misc/scripts/black_format.sh
+++ misc/scripts/black_format.sh
@@ -15,6 +15,7 @@ fi
 echo -e "Formatting Python files..."
 PY_FILES=$($find \( -path "./.git" \
                 -o -path "./thirdparty" \
+                -o -path "./modules/gd_*" \
                 \) -prune \
                 -o \( -name "SConstruct" \
                 -o -name "SCsub" \
