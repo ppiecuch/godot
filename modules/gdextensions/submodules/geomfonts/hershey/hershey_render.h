0c075a88103e2b694f69e4c9b5ae1542a22012b6
Pawel Piecuch <piecuch.pawel@gmail.com>
2022-06-17 15:01:25 +0200

    cleanup formatting issues

--- modules/gdextensions/submodules/geomfonts/hershey/hershey_render.h
+++ modules/gdextensions/submodules/geomfonts/hershey/hershey_render.h
@@ -51,37 +51,35 @@
 
 #include <vector>
 
-#include "core/variant.h"
 #include "core/error_macros.h"
 #include "core/math/math_funcs.h"
 #include "core/math/vector2.h"
+#include "core/variant.h"
 
 #define as_real_t(v) static_cast<real_t>(v)
 
-// Structure use in letters animation.
-struct hershey_string_t {
-	real_t dx, dy; // segment unit extending
-	Vector2 begin, end; // animated line end coords
-	hershey_string_t(int16_t x1, int16_t y1, int16_t x2, int16_t y2) :
-			begin({ as_real_t(x1), as_real_t(y1) }), end({ as_real_t(x2), as_real_t(y2) }) {
+// Structure use in letters animation - animated segment
+struct hershey_seg_t {
+	Vector2 begin, end, delta /* segment unit extending */;
+	hershey_seg_t(real_t x1, real_t y1, real_t x2, real_t y2) :
+			begin({ x1, y1 }), end({ x2, y2 }) {
 		const real_t angle = Math::atan2(real_t(y2 - y1), real_t(x2 - x1));
-		dx = Math::cos(angle);
-		dy = Math::sin(angle);
+		delta = { Math::cos(angle), Math::sin(angle) };
+	}
+	void step() {
+		begin += delta;
 	}
 };
 
 inline static int hershey_string_width(const char *s, const char *font_data[], const int font_data_size[], const char font_width[]) {
 	ERR_FAIL_NULL_V(s, 0);
 
-	int16_t w = 0;
+	int w = 0;
 	while (*s) {
 		w += font_width[*s++ - ' '];
 	}
 	return w;
 }
-
-/// Making geometry
-/// ---------------
 
 // make vertices array and returns the number of vertices
 
@@ -89,9 +87,9 @@ inline static int hershey_make(real_t x, real_t y, const char *s, const char *fo
 	int w = 0;
 	while (*s) {
 		int index = *s++ - ' ', size = font_data_size[index];
-		DEV_ASSERT(size%2 == 0);
+		DEV_ASSERT(size % 2 == 0);
 		for (int vert = 0; vert < size; vert += 2) {
-			out_data.push_back({x + font_data[index][vert] + w, y - font_data[index][vert+1]});
+			out_data.push_back({ x + font_data[index][vert] + w, y - font_data[index][vert + 1] });
 		}
 		w += font_width[index];
 	}
@@ -105,9 +103,9 @@ inline static int hershey_make(real_t x, real_t y, real_t scale, const char *s, 
 	int w = 0;
 	while (*s) {
 		int index = *s++ - ' ', size = font_data_size[index];
-		DEV_ASSERT(size%2 == 0);
+		DEV_ASSERT(size % 2 == 0);
 		for (int vert = 0; vert < size; vert += 2) {
-			out_data.push_back({x + font_data[index][vert] + w, y - font_data[index][vert+1]});
+			out_data.push_back({ x + font_data[index][vert] + w, y - font_data[index][vert + 1] });
 		}
 		w += font_width[index];
 	}
@@ -121,9 +119,9 @@ inline static int hershey_make(const char *s, const char *font_data[], const int
 	int w = 0;
 	while (*s) {
 		int index = *s++ - ' ', size = font_data_size[index];
-		DEV_ASSERT(size%2 == 0);
+		DEV_ASSERT(size % 2 == 0);
 		for (int vert = 0; vert < size; vert += 2) {
-			out_data.push_back({as_real_t(font_data[index][vert]), as_real_t(font_data[index][vert+1])});
+			out_data.push_back({ as_real_t(font_data[index][vert]), as_real_t(font_data[index][vert + 1]) });
 		}
 		w += font_width[index];
 	}
@@ -135,17 +133,17 @@ inline static int hershey_make(const char *s, const char *font_data[], const int
 
 // make segments array and returns the number of segments (two vertices for segment)
 
-inline static int hershey_make(const char *s, const char *font_data[], const int font_data_size[], const char font_width[], std::vector<hershey_string_t> &out_data, int *out_width = nullptr) {
+inline static int hershey_make(const char *s, const char *font_data[], const int font_data_size[], const char font_width[], std::vector<hershey_seg_t> &out_data, int *out_width = nullptr) {
 	out_data.reserve(256);
 	int w = 0;
 	while (*s) {
 		int index = *s++ - ' ', size = font_data_size[index];
+		DEV_ASSERT(size % 4 == 0);
 		while ((size -= 4) >= 0)
-			out_data.push_back(hershey_string_t(
-					font_data[index][size] + w, // offset x coord
-					-font_data[index][size + 1],
-					font_data[index][size + 2] + w, // offset x coord
-					-font_data[index][size + 3]));
+			out_data.push_back({ as_real_t(font_data[index][size] + w), // offset x coord
+					as_real_t(-font_data[index][size + 1]),
+					as_real_t(font_data[index][size + 2] + w), // offset x coord
+					as_real_t(-font_data[index][size + 3]) });
 		w += font_width[index];
 	}
 	if (out_width) {
