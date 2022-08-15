1008d17f3e7b351674114c2e01157b3c3f903b0c
Pawel Piecuch <piecuch.pawel@gmail.com>
2022-08-15 16:56:55 +0200

    playfab api placeholder

--- modules/gdextensions/submodules/playfab/gdplayfab.h
+++ modules/gdextensions/submodules/playfab/gdplayfab.h
@@ -10,5 +10,21 @@ class PlayFab : public Object {
 public:
 	PlayFab *get_instance();
 
+	// https://docs.microsoft.com/en-us/rest/api/playfab/admin/?view=playfab-rest
+
+	// Account Management
+	// Authentication
+	// Characters
+	// Content
+	// Custom Server Management
+	// Matchmaking
+	// Play Stream
+	// Player Data Management
+	// Player Item Management
+	// Scheduled Task
+	// Server-Side Cloud Script
+	// Shared Group Data
+	// Title-Wide Data Management
+
 	PlayFab();
 };
