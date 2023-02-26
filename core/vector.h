3c519b84a2037eb29737543c0aa4349ec5375c2a
Pawel Piecuch <piecuch.pawel@gmail.com>
2023-02-26 17:09:22 +0100

    add back method for convinience

--- core/vector.h
+++ core/vector.h
@@ -123,6 +123,7 @@ public:
 	_FORCE_INLINE_ void set(int p_index, const T &p_elem) { _cowdata.set(p_index, p_elem); }
 	_FORCE_INLINE_ int size() const { return _cowdata.size(); }
 	_FORCE_INLINE_ const T &last(int p_index = 0) const { return _cowdata.get(_cowdata.size() - 1 - p_index); }
+	_FORCE_INLINE_ T back(int p_index = 0) { return _cowdata.get(_cowdata.size() - 1 - p_index); }
 	Error resize(int p_size) { return _cowdata.resize(p_size); }
 	_FORCE_INLINE_ const T &operator[](int p_index) const { return _cowdata.get(p_index); }
 	Error insert(int p_pos, T p_val) { return _cowdata.insert(p_pos, p_val); }
