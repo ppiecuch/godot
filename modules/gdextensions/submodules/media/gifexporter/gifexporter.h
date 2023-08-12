13b23347f6030138e5ce67baa597d5890d5cabc0
Pawel Piecuch <piecuch.pawel@gmail.com>
2023-08-12 21:12:33 +0200

    cleanup; update code formatting

--- modules/gdextensions/submodules/media/gifexporter/gifexporter.h
+++ modules/gdextensions/submodules/media/gifexporter/gifexporter.h
@@ -43,16 +43,18 @@ class GifExporter : public Reference {
 	GifAnim ganim;
 	GifWriter gwriter;
 
+	String filename;
+
 protected:
 	static void _bind_methods();
 
 public:
-	String filename;
-
 	void set_filename(const String file);
-	void begin_export(const Size2 &size, float frame_delay, int loop_count = 0, int32_t bit_depth = 8, bool dither = false);
+	String get_filename() const;
+
+	Error begin_export(const Size2 &size, float frame_delay, int loop_count = 0, int32_t bit_depth = 8, bool dither = false);
+	Error write_frame(const Ref<Image> frame, const Color &background_color, float frame_delay, int32_t bit_depth = 8, bool dither = false);
 	void end_export();
-	void write_frame(const Ref<Image> frame, const Color &background_color, float frame_delay, int32_t bit_depth = 8, bool dither = false);
 
 	GifExporter();
 	~GifExporter();
