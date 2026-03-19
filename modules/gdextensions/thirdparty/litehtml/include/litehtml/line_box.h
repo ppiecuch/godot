b7effd07a8ee0a361c74559b6d3df098c785aa8b
Pawel Piecuch <piecuch.pawel@gmail.com>
2026-03-19 18:36:28 +0100

    Cleanup build issues

--- modules/gdextensions/thirdparty/litehtml/include/litehtml/line_box.h
+++ modules/gdextensions/thirdparty/litehtml/include/litehtml/line_box.h
@@ -42,6 +42,7 @@ namespace litehtml
 		int m_rendered_min_width;
 	public:
 		explicit line_box_item(const std::shared_ptr<render_item>& element) : m_element(element), m_rendered_min_width(0) {}
+		virtual ~line_box_item() = default;
 		line_box_item() = default;
 		line_box_item(const line_box_item& el) = default;
 		line_box_item(line_box_item&&) = default;
@@ -130,10 +131,10 @@ namespace litehtml
 				m_right(right),
 				m_height(0),
 				m_width(0),
-				m_font_metrics(fm),
-				m_default_line_height(line_height),
-				m_baseline(0),
 				m_line_height(0),
+				m_default_line_height(line_height),
+				m_font_metrics(fm),
+				m_baseline(0),
 				m_text_align(align),
 				m_min_width(0)
 		{
